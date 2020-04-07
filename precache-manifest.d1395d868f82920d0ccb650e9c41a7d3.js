ccf56cbecabd4a99bb53fdea4bccab0bf0c10281
Max W. Portocarrero <mxportocarrero@gmail.com>
2020-04-07 01:02:32 -0500

    Updates

---
+++ precache-manifest.d1395d868f82920d0ccb650e9c41a7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b316f4ef3559e748745c4ea6fa4b4f01",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "8a3518708bffbaad894a",
+    "url": "/react1/static/css/main.62e11da6.chunk.css"
+  },
+  {
+    "revision": "d75db5fa38bb8011da20",
+    "url": "/react1/static/js/2.7f3818e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react1/static/js/2.7f3818e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3518708bffbaad894a",
+    "url": "/react1/static/js/main.9d3ccdce.chunk.js"
+  },
+  {
+    "revision": "e55a1e5e5e81339a9bfa",
+    "url": "/react1/static/js/runtime-main.ab2957d0.js"
+  }
+]);
